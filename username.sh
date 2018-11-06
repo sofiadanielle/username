5a7c715e9e6b0937118d3a94e52228d800ed223a
Sofia Bejerano <44761938+sofiadanielle@users.noreply.github.com>
2018-11-06 10:50:39 -0800

    Update username.sh

--- username.sh
+++ username.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 # username.sh
-echo "Enter a username: "
-read ZIP
-while echo $ZIP | egrep -v "^[0-9]{3,12}$" > /dev/null 2>&1
+echo "Enter a username that includes an underscore, is all lowercase, and is 3 to 12 characters long: "
+read username
+while echo $username | egrep -v "^[a-z][a-z0-9_]{3,12}$" > /dev/null 2>&1
 do
-	echo "You must enter a valid ZIP code - five digits only!"
-	echo "Enter a five-digit ZIP code: "
-	read ZIP
+	echo "You must enter a valid username!"
+	echo "Enter a username that includes an underscore, is all lowercase, and is 3 to 12 characters long: "
+	read username
 done
 echo "Thank you"
 
