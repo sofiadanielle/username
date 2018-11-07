a41c810d1e20fc59df0141746223b68db54fa1a4
Sofia Bejerano <44761938+sofiadanielle@users.noreply.github.com>
2018-11-06 19:22:37 -0800

    Update username.sh

--- username.sh
+++ username.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 # username.sh
-echo "Enter a username taht is all lowercase, and is 3 to 12 characters long (can include underscores!): "
+echo "Enter a username that is all lowercase, starts with a letter, and is 3 to 12 characters long (can include underscores and numbers!): "
 read username
 while echo $username | egrep -v "^[a-z][a-z0-9_]{3,12}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid username!"
-	echo "Enter a username that is all lowercase, and is 3 to 12 characters long (can include underscores!: "
+	echo "Enter a username that is all lowercase, and is 3 to 12 characters long (can include underscores and numbers!): "
 	read username
 done
 echo "Thank you"
